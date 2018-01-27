78500352f4115d4e5c79397005949bb0f0c17c5c
abozanona <abozanona@gmail.com>
2018-01-27 13:09:22 +0200

    initial commit

---
+++ Assets/Scripts/GamePlay.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlay : MonoBehaviour {
+
+    public float rate;
+    public List<GameObject> points;
+    public GameObject pointPrefab;
+    public GameObject player;
+
+    public int playerPosition;
+    public double score = 0;
+
+    // Use this for initialization
+    void Start () {
+        rate = 1f;
+        GameObject obj;
+        Vector3 pos;
+
+        float[] yPos = { 0.77f, 3.8f, 6.0f, 9.1f, 10.2f };
+        for(int i = 0;i < yPos.Length; i++){
+            obj = Instantiate(pointPrefab) as GameObject;
+            pos = new Vector3(Random.Range(-3.3f, 3.3f), yPos[i], 0);
+            obj.GetComponent<Transform>().position = pos;
+            obj.GetComponent<Point>().number = i;
+            points.Add(obj);
+        }
+        player.transform.position = points[0].transform.position;
+        playerPosition = 0;
+        Time.timeScale = rate;
+        Invoke("startSpeedAtOne", 10);
+    }
+
+    void startSpeedAtOne()
+    {
+        rate = 1.3f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        #region remove points out of screen, update player position
+        if (!player.GetComponent<Player>().isMoving)
+        {
+            player.transform.position = points[playerPosition].transform.position;
+        }
+        Time.timeScale += 0.00001f;
+        for (int i=0;i< points.Count;i++)
+        {
+            Vector3 newPos = points[i].GetComponent<Transform>().position;
+            newPos.y-= 1 * Time.deltaTime;
+            points[i].GetComponent<Transform>().position = newPos;
+            if (points[i].GetComponent<Transform>().position.y < -6)
+            {
+                if(i == playerPosition)
+                {
+                    //todo: show game over here
+                }
+                //find max y point
+                float maxY = -999;
+                for(int j = 0; j < points.Count; j++)
+                {
+                    if (points[j].GetComponent<Transform>().position.y > maxY)
+                    {
+                        maxY = points[j].GetComponent<Transform>().position.y;
+                    }
+                }
+
+                Destroy(points[i]);
+                Vector3 pos = new Vector3(Random.Range(-3.3f, 3.3f), Random.Range(maxY + 2, maxY + 5), 0);
+                points[i] = Instantiate(pointPrefab) as GameObject;
+                points[i].GetComponent<Transform>().position = pos;
+                points[i].GetComponent<Point>().number = i;
+            }
+        }
+        #endregion
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), "Score: " + score);
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+    public GameObject direction;
+    float angle;
+    public bool isMoving = false;
+    public int remainingBounces = 3;
+    public Rigidbody2D rb;
+
+    private GamePlay gamePlay;
+
+    public GameObject gameoverCanvas;
+    public GameObject gameoverText;
+
+    public bool hasColided = false;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        gamePlay = Camera.main.gameObject.GetComponent<GamePlay>();
+    }
+	
+    public float getMinimumDistanceIsAvailable()
+    {
+        return Vector3.Distance(transform.position, gamePlay.points[gamePlay.playerPosition].transform.position);
+    }
+    // Update is called once per frame
+    bool isUp = false;
+    void Update () {
+        //float angle = Mathf.PingPong(Time.time, 140) - 70;
+        float value = 1.3f;
+        if (!isMoving)
+        {
+            //angle = Mathf.Sin(Time.time) * 70;
+            if (isUp)
+            {
+                angle += value;
+                if (angle > 70)
+                {
+                    isUp = false;
+                }
+            }
+            else
+            {
+                angle -= value;
+                if (angle < -70)
+                {
+                    isUp = true;
+                }
+            }
+            direction.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+        if ((Input.touchCount > 0 || Input.GetMouseButtonDown(0)) && (!isMoving))
+        {
+            isMoving = true;
+            rb.AddForce(direction.transform.up * 8000 * Time.deltaTime);
+        }
+
+        if (hasColided)
+        {
+            Vector2 curPos = new Vector2(transform.position.x, transform.position.y);
+            Vector2 newPos = gamePlay.points[gamePlay.playerPosition].transform.position;
+
+            transform.position = Vector2.MoveTowards(curPos, newPos, 5f * Time.deltaTime);
+
+            if (getMinimumDistanceIsAvailable() < 0.3f)
+            {
+                hasColided = false;
+                isMoving = false;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Point" && !hasColided)
+        {
+            if(coll.gameObject.GetComponent<Point>().number == gamePlay.playerPosition)
+            {
+                return;
+            }
+            gamePlay.score++;
+            hasColided = true;
+            int newPointNumber = coll.gameObject.GetComponent<Point>().number;
+            gamePlay.playerPosition = newPointNumber;
+            Destroy(coll.gameObject.GetComponent<CircleCollider2D>());
+        }
+        else if(coll.gameObject.tag == "Death")
+        {
+            gameoverCanvas.SetActive(true);
+            gameoverText.GetComponent<Text>().text = "Game over\nScore: " + gamePlay.score;
+            Time.timeScale = 0;
+            //todo: show game over here
+        }
+    }
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Out")
+        {
+            gameoverCanvas.SetActive(true);
+            gameoverText.GetComponent<Text>().text = "Game over\nScore: " + gamePlay.score;
+            Time.timeScale = 0;
+            //todo: show game over here
+        }
+    }
+}
