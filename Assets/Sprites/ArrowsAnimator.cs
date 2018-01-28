3942ed58216def07bf77540118e2e1213f4641ce
abozanona <abozanona@gmail.com>
2018-01-28 10:49:33 +0200

    new packup

---
+++ Assets/Sprites/ArrowsAnimator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ArrowsAnimator : MonoBehaviour {
+    public Texture2D[] arrowssprites;
+    public GameObject viewer;
+    public float duration;
+    private float time;
+    private int index = 0;
+	// Use this for initialization
+	void Start () {
+        time = duration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (duration>0) {
+            duration -= Time.deltaTime;
+        }
+        else
+        {
+            viewer.GetComponent<RawImage>().texture = arrowssprites[index % 3];
+            index += 1;
+
+            duration = time;
+        }
+
+        if (index == 17) index = 0;
+
+    }
+}
---
+++ Assets/adbka/Assets/s&p&ee/Mainmenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class Mainmenu : MonoBehaviour {
+    public Animator anim;
+    public void playGame()
+    {   if(anim.GetInteger("Selected")==0)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void back()
+    {
+
+        SceneManager.LoadScene("menu");
+
+    }
+    public void op()
+    {
+
+
+        SceneManager.LoadScene("s");
+    }
+    public void QuitGame()
+    {
+        if (anim.GetInteger("Selected") == 3)
+        {
+            Debug.Log("quit the game!!");
+            Application.Quit();
+        }
+    }
+   public void setto(int i)
+    {
+        bool dir = (i + 3-1 == anim.GetInteger("Selected") + 3)|| !(i==3 && anim.GetInteger("Selected")==0);
+        anim.SetBool("ToRight", dir);
+        anim.SetInteger("Selected", i);
+    }
+}
